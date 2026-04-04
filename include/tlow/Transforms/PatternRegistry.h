48c222ca8816561484683bcaf727547f8edb9ccb
dsuarez01 <dsuarez@mit.edu>
2026-04-03 23:45:36 -0400

    Initial work on transforms/codegen

--- include/tlow/Transforms/PatternRegistry.h
+++ include/tlow/Transforms/PatternRegistry.h
@@ -1,1 +1,11 @@
+#pragma once
 // collection of rewrite patterns
+
+#include "mlir/Pass/Pass.h"
+#include <memory>
+
+namespace tlow {
+
+std::unique_ptr<mlir::Pass> createLowerTlowToLinalgPass();
+
+} // namespace tlow
